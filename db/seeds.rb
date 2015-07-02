2b5544bb137775715b1087788ed83769cdbc22f2
Bradley Gohman <bradley.s.gohman@gmail.com>
2015-07-02 08:34:25 -0500

    add wiki model and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Wiki.destroy_all 
+
+ require 'faker'
+
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+ # Create Wikis
+ 15.times do
+  Wiki.create!(
+    title:        Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph,
+    private: false
+  )
+ end
+ 
+
+
+ 
+ puts "Seed finished"
+ puts "#{Wiki.count} wikis created"
+ puts "#{User.count} users created"
