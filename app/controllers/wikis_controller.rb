d4c8abdb6b62f1c5a0a91f8350c403b62561b377
Bradley Gohman <bradley.s.gohman@gmail.com>
2015-07-02 09:49:42 -0500

    creating new wikis

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -8,8 +8,26 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new(wiki_params)
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the wiki.  Please try again."
+      render :new
+    end
   end
 
   def edit
   end
+
+  private
+
+    def wiki_params
+      params.require(:wiki).permit(:title, :body, :private)
+    end
 end
