56cf8baab0493e72b77149303616e0de94dbf678
Bradley Gohman <bradley.s.gohman@gmail.com>
2015-10-09 15:25:06 -0500

    more controller specs

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe WikisController, type: :controller do
       @user = create(:user)
       sign_in(@user)
     end
+
     describe 'POST create' do
       it 'creates a new wiki' do
         expect {
@@ -21,6 +22,61 @@ RSpec.describe WikisController, type: :controller do
         post :create, wiki: attributes_for(:wiki)
         expect(response).to redirect_to(assigns[:wiki])
       end
+    end
+
+    describe 'GET index' do
+      it 'assigns viewable wikis to @wikis' do
+        public_wiki = create(:wiki)
+        get :index
+        expect(assigns(:wikis)).to include(public_wiki)
+      end
+      it 'does not include private wikis' do
+        private_wiki = create(:wiki, private: true)
+        get :index
+        expect(assigns(:wikis)).not_to include(private_wiki)
+      end
+      #https://www.relishapp.com/rspec/rspec-rails/docs/matchers/render-template-matcher
+      #it "renders the index template" do
+      #  subject { get :index }
+      #  expect(subject).to render_template(:index)
+      #end
+    end
+
+    describe 'GET new' do
+      it 'assigns a new wiki to @wiki' do
+        get :new
+        expect(assigns(:wiki)).to be_a_new(Wiki)
+      end
+      it 'assigns current_user to @user' do
+        get :new
+        expect(assigns(:user)).to eq(@user)
+      end
+    end
+
+    describe 'GET edit' do
+      it 'assigns the selected wiki to @wiki' do
+        get :edit, { id: my_wiki.to_param }
+        expect(assigns(:wiki)).to eq(my_wiki)
+      end
+    end
+
+    describe 'GET show' do
+      it 'assigns wiki to @wiki' do
+        get :show, { id: my_wiki.to_param }
+        expect(assigns(:wiki)).to eq(my_wiki)
+      end
+    end
+
+    describe 'PATCH update' do
+
+    end
+
+    describe 'PUT update' do
+
+    end
+
+    describe 'DELETE destroy' do
+
     end
   end
 end
